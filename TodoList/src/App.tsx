8876cf156d770ef59d2b5607962b7ad885904fbf
EduardoD048 <eduardo.demetrio2000@gmail.com>
2024-02-10 17:12:00 -0300

    feature: components

--- TodoList/src/App.tsx
+++ TodoList/src/App.tsx
@@ -2,23 +2,44 @@
 import Footer from "./components/Footer";
 import Header from "./components/Heade";
 import TaskForm from "./components/TaskForm";
-import TaskList from "./components/TaskList"
+import TaskList from "./components/TaskList";
+import Modal from "./components/Modal";
+
+import { useState } from "react";
 
 // css
 import styles from "./App.module.css";
 
+// interface
+import { ITask } from "./interfaces/Task";
+
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id;
+      })
+    );
+  };
+
   return (
     <div>
+      <Modal children={<TaskForm btnText="Editar Tarefa" taskList={taskList}/>}/>
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer</h2>
-          <TaskForm btnText="Criar Tarefa" />
+          <TaskForm
+            btnText="Criar Tarefa"
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList />
+          <TaskList taskList={taskList} handleDelete={deleteTask} />
         </div>
       </main>
       <Footer />
--- TodoList/src/components/TaskForm.tsx
+++ TodoList/src/components/TaskForm.tsx
@@ -1,24 +1,72 @@
-import * as React from "react";
+import react, { useState, ChangeEvent, FormEvent, useEffect } from "react";
 
 //css
-import styles from "./TaskForm.module.css"
+import styles from "./TaskForm.module.css";
+
+//interface
+import { ITask } from "../interfaces/Task"; // 2 pontos para voltar 1 pasta
 
 interface Props {
-  btnText: string
+  btnText: string;
+  taskList: ITask[];
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>;
 }
 
-const TaskForm = ({btnText}: Props) => {
-  return <form className={styles.form}>
-    <div className={styles.input_container}>
-      <label htmlFor="title">título</label>
-      <input type="text" name="title" placeholder="título da tarefa" />
-    </div>
-    <div className={styles.input_container}>
-      <label htmlFor="dificulty">dificuldade</label>
-      <input type="text" name="title" placeholder="Dificuldade da tarefa" />
-    </div>
-    <input type="submit" value={btnText}/>
-  </form>;
+const TaskForm = ({ btnText, taskList, setTaskList }: Props) => {
+  const [id, setId] = useState<number>(0);
+  const [title, setTitle] = useState<string>("");
+  const [dificulty, setDificulty] = useState<number>(0);
+
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    // func que realiza a ação de inclusão de tarefas no sistema
+    e.preventDefault();
+
+    const id = Math.floor(Math.random() * 1000);
+
+    const newTask: ITask = { id, title, dificulty };
+
+    setTaskList!([...taskList, newTask]);
+
+    setTitle("");
+    setDificulty(0);
+
+    console.log(taskList);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // func vai manuzear as alterações do input
+    if (e.target.name === "title") {
+      setTitle(e.target.value);
+    } else {
+      setDificulty(parseInt(e.target.value));
+    }
+  };
+
+  return (
+    <form onSubmit={addTaskHandler} className={styles.form}>
+      <div className={styles.input_container}>
+        <label htmlFor="title">título</label>
+        <input
+          type="text"
+          name="title"
+          placeholder="título da tarefa"
+          onChange={handleChange}
+          value={title}
+        />
+      </div>
+      <div className={styles.input_container}>
+        <label htmlFor="dificulty">dificuldade</label>
+        <input
+          type="text"
+          name="dificulty"
+          placeholder="Dificuldade da tarefa"
+          onChange={handleChange}
+          value={dificulty}
+        />
+      </div>
+      <input type="submit" value={btnText} />
+    </form>
+  );
 };
 
 export default TaskForm;
